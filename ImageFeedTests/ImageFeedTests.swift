82ad6bdbd96c7952e4fad6ad202bf3557c19d95d
Svetlik-B <svetlik.igorevna@gmail.com>
2024-12-21 19:44:07 +0400

    создать xcode проект

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Svetlana Bochkareva on 21.12.2024.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
