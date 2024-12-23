3baeaf0f907c684082c86b5f0886a2b86f8f9aaa
Svetlik-B <svetlik.igorevna@gmail.com>
2024-12-23 19:28:25 +0400

    создать таблицу и ImagesListViewController

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Svetlana Bochkareva on 21.12.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
