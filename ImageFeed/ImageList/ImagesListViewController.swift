14e46dd98e9c362c58e1786300bb1fc0641ffb9d
Svetlik-B <svetlik.igorevna@gmail.com>
2024-12-27 17:24:48 +0400

    убрать выделение ячейки

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class ImagesListViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        tableView.allowsSelection = false
     }
 }
 // MARK: - UITableViewDataSource
