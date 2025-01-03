8419b4fecf7e6278a421a19aa7f059c32dc1efa4
Svetlik-B <svetlik.igorevna@gmail.com>
2025-01-03 14:44:47 +0400

    Сверстать ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var photoImageView: UIImageView!
+    
+    @IBOutlet private var buttonEntrance: UIButton!
+    
+    @IBAction private func buttonLogin() {
+    }
+    
+}
