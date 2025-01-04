ba8b341f42a81af41919b3bf98d4350dbcb0f01d
Svetlik-B <svetlik.igorevna@gmail.com>
2025-01-04 16:34:03 +0400

    переход от экрана с таблицей к экрану с одной картинкой

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -5,7 +5,6 @@ final class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-        tableView.allowsSelection = false
     }
 }
 // MARK: - UITableViewDataSource
