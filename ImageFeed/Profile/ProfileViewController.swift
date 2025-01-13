65837a2c94f3dc5d2b378a5c5013d300a2050b23
Svetlik-B <svetlik.igorevna@gmail.com>
2025-01-13 20:30:22 +0400

    сверстать ProfileViewController кодом

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,14 +1,71 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginLabel: UILabel!
-    @IBOutlet private var textLabel: UILabel!
-    @IBOutlet private var photoImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(named: "YP Black")
+        
+        let photoImageView = UIImage(named: "Photo")
+        let imageView = UIImageView(image: photoImageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+        imageView.widthAnchor.constraint(equalToConstant: 70),
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+        ])
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        nameLabel.textColor = .white
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+        ])
+        
+        let loginLabel = UILabel()
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.textColor = .white
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        NSLayoutConstraint.activate([
+            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)
+            ])
+        
+        let textLabel = UILabel()
+        textLabel.text = "Hello, World!"
+        textLabel.font = UIFont.systemFont(ofSize: 13)
+        textLabel.textColor = .white
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            ])
     
-    @IBOutlet private var buttonEntrance: UIButton!
-    
-    @IBAction private func buttonLogin() {
+        let buttonEntrance = UIButton.systemButton(
+            with: UIImage(named: "Exit")!,
+            target: self,
+            action: #selector(buttonLogin))
+        buttonEntrance.tintColor = .red
+        buttonEntrance.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonEntrance)
+        NSLayoutConstraint.activate([
+            buttonEntrance.widthAnchor.constraint(equalToConstant: 44),
+            buttonEntrance.heightAnchor.constraint(equalToConstant: 44),
+            buttonEntrance.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide
+                .trailingAnchor, constant: -16),
+            buttonEntrance.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ])
     }
     
+    @objc
+    private func buttonLogin() {}
 }
