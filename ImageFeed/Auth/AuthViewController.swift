0b2961f2df400f4638008752d0e2c188c5fe7932
Svetlik-B <svetlik.igorevna@gmail.com>
2025-01-19 20:36:44 +0400

    получить код доступа

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,6 +1,24 @@
 import Foundation
 import UIKit
 
+private let showWebViewSegueIdentifier = "ShowWebView"
+
 class AuthViewController: UIViewController {
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            if let vc = segue.destination as? WebViewViewController {
+                vc.delegate = self
+            }
+        }
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(
+        _ vc: WebViewViewController,
+        didAuthenticateWithCode code: String
+    ) {
+        navigationController?.popViewController(animated: true)
+    }
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {}
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -2,18 +2,77 @@ import Foundation
 import UIKit
 import WebKit
 
-class WebViewViewController: UIViewController {
-    let showWebViewSegueIdentifier = "ShowWebView"
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
 
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(
+        _ vc: WebViewViewController,
+        didAuthenticateWithCode code: String
+    )
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
+class WebViewViewController: UIViewController {
     @IBOutlet private var webView: WKWebView!
+    weak var delegate: WebViewViewControllerDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         configureBackButton()
+        loadAuthView()
     }
 }
 
-private extension WebViewViewController {
-    func configureBackButton() {
+// MARK: - WKNavigationDelegate
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) ->
+            Void
+    ) {
+        if let code = code(from: navigationAction) {
+            decisionHandler(.cancel)
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+}
+
+// MARK: - Implementation
+extension WebViewViewController {
+    fileprivate func code(from navigationAction: WKNavigationAction) -> String? {
+        if let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+    fileprivate func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)
+        else { return }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope),
+        ]
+        guard let url = urlComponents.url
+        else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+
+    fileprivate func configureBackButton() {
         // TODO: исправить код замены back button
         navigationController?.navigationBar.backIndicatorImage = UIImage(
             named: "Backward")
