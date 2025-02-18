5d11597257174a933a03fa9edeca07557346944e
Svetlik-B <svetlik.igorevna@gmail.com>
2025-02-18 12:28:20 +0400

    добавить tab_editorial_no_active

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -7,6 +7,12 @@ final class TabBarController: UITabBarController {
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let imagesListViewController = storyboard.instantiateViewController(
             withIdentifier: "ImagesListViewController")
+        
+        imagesListViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_editorial_no_active")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal),
+            selectedImage: UIImage(named: "tab_editorial_active")
+        )
         let profileViewController = ProfileViewController()
         profileViewController.tabBarItem = UITabBarItem(
             title: "",
