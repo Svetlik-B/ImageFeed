aced35d411026234273e6d3ef178a203180df668
Svetlik-B <svetlik.igorevna@gmail.com>
2025-03-09 16:39:35 +0400

    ProfileViewPresenter

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -14,6 +14,8 @@ final class TabBarController: UITabBarController {
             selectedImage: UIImage(named: "tab_editorial_active")
         )
         let profileViewController = ProfileViewController()
+        profileViewController.presenter = ProfileViewPresenter(view: profileViewController)
+        
         profileViewController.tabBarItem = UITabBarItem(
             title: "",
             image: UIImage(named: "tab_profile_no_active")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal),
